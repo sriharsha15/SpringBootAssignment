b0ba9a7bec2b7b6bc26d27c23e369f244ec6ee94
SRIHARSHA ADDALA <37024300+sriharsha15@users.noreply.github.com>
2020-04-15 13:20:16 -0400

    Add files via upload

---
+++ Spring Lab 4/src/main/java/com/cg/dao/TraineeDaoI.java
@@ -0,0 +1,21 @@
+package com.cg.dao;
+
+import java.util.List;
+
+import com.cg.entity.Trainee;
+
+
+public interface TraineeDaoI 
+{
+
+	public void add(Trainee trainee);
+
+	public List<Trainee> reterive();
+
+	public Trainee findById(int traineeId);
+
+	public void delete(int traineeId);
+
+	public void modify(Trainee trainee);
+	public Boolean delet(int traineeId);
+}
